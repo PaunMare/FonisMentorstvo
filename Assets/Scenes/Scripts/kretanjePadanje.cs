1cb12368ccd6024795aca5460bf0a41e30ba6d81
MareP-hub <74553764+MareP-hub@users.noreply.github.com>
2020-11-17 23:57:46 +0100

    Odradjeno kretanje

--- Assets/Scenes/Scripts/kretanjePadanje.cs
+++ Assets/Scenes/Scripts/kretanjePadanje.cs
@@ -6,18 +6,34 @@ public class kretanjePadanje : MonoBehaviour
 {
 
     public float moveSpeed = 30f;
-    
+
+    public Sprite left, right;
+  
+    public SpriteRenderer player;
+
+    private void Awake()
+    {
+        player.sprite = right;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.sprite = left;
+
+        }
+        else if(Input.GetKeyDown(KeyCode.D)){
+            player.sprite = right;
+        }
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
