2608854364b026afad330b618a7a26ebdcc46b1c
MareP-hub <74553764+MareP-hub@users.noreply.github.com>
2020-11-17 22:43:29 +0100

    Provera

---
+++ Assets/Scenes/Scripts/kretanjePadanje.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kretanjePadanje : MonoBehaviour
+{
+
+    public Vector2 brzina = new Vector2(50, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 kretanje = new Vector3(brzina.x*inputX,0);
+        transform.Translate(kretanje);
+    }
+}
